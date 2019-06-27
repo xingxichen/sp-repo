ea17bdb5434a87ffb1e821f48ee8846aa033c918
xingxichen <xingxichen@users.noreply.github.com>
2019-06-27 17:35:36 +0800

    init

---
+++ sp09-feign/src/main/java/com/tedu/sp09/Sp09FeignApplication.java
@@ -0,0 +1,18 @@
+package com.tedu.sp09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+//@EnableCircuitBreaker
+//@EnableDiscoveryClient
+//@SpringBootApplication
+@EnableFeignClients
+@SpringCloudApplication
+public class Sp09FeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp09FeignApplication.class, args);
+	}
+
+}
